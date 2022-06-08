5c842f8bc69ad3ecf456b767fbd2f6b83041e3ed
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-08 13:47:23 +0900

    行を.mapでループさせる

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,33 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [price, setPrice] = useState({
-    tanka: "1000",
-    suuryou: "3",
-    tani: "ページ"
-  });
+  const [price, setPrice] = useState([
+    {
+      koumoku: "企画費",
+      tanka: "1000",
+      suuryou: "3",
+      tani: "ページ"
+    },
+    {
+      koumoku: "デザイン費",
+      tanka: "1000",
+      suuryou: "3",
+      tani: "ページ"
+    }
+  ]);
 
   return (
     <div className="App">
-      <div>
-        <p></p>
-        <p></p>
-        <p></p>
-      </div>
+      {price.map((s) => {
+        return (
+          <div key={s}>
+            <p>{s.koumoku}</p>
+            <p>{s.tanka}</p>
+            <p>{s.suuryou}</p>
+            <p>{s.tani}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
