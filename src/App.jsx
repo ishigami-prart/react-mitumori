6afe68de1ec9a60b2b35c25c3ee153fa05f5ae1b
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-08 13:30:23 +0900

    01

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import React, { useState } from "react";
 import "./styles.css";
-import { InputPrice } from "./components/inputPrice";
-import { PriceList } from "./components/priceList";
 
 export default function App() {
-  const [priceText, setPriceText] = useState("");
-  const onChangePriceText = (event) => setPriceText(event.target.value);
-  const onClickAdd = () => {
-    console.log(onChangePriceText);
-  };
-  const priceLists
+  const [price, setPrice] = useState({
+    tanka: "1000",
+    suuryou: "3",
+    tani: "ページ"
+  });
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <InputPrice
-        priceText={priceText}
-        onChange={onChangePriceText}
-        onClick={onClickAdd}
-      />
-      <PriceList priceLists={priceLists} />
+      <div>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
     </div>
   );
 }
